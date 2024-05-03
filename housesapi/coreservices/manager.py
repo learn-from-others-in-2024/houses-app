43ebe65c43ff89383578bfea6f8c74c1391eb3cf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-03 18:18:34 +0530

    Create manager.py

---
+++ housesapi/coreservices/manager.py
@@ -0,0 +1,14 @@
+from core import core
+from core import db
+from flask_migrate import Migrate
+from flask_migrate import MigrateCommand
+from flask_script import Manager
+
+
+migrate = Migrate(core, db)
+
+manager = Manager(core)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
