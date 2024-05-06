8cacee8ee23398133941e8bf3cd906be4615cf81
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-06 15:44:28 +0530

    Changes

---
+++ housesapi/housesservices/houses/producer.py
@@ -0,0 +1,18 @@
+import pika
+import json
+
+
+# Input your rabbitmq url
+params = pika.URLParameters('{{Your_AMQP_URL}}')
+
+# Create a connection
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+# Create a publisher
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='core',
+                          body=json.dumps(body), properties=properties)
