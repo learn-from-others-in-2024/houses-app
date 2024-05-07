9b0b46c6a7b02a95a907a6eda8badbbf72204ed4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-07 12:51:59 +0530

    Changes

--- housesapi/coreservices/core.py
+++ housesapi/coreservices/core.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
 from flask_migrate import Migrate
 from dataclasses import dataclass
 from flask_cors import CORS
+from dataclasses import dataclass
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -28,9 +31,64 @@ class House(db.Model):
     description = db.Column(db.String(150))
 
 
+# Creating the HouseChecker model:
+@dataclass
+class HouseChecker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    checker_id = db.Column(db.Integer)
+    house_id = db.Column(db.Integer)
+
+    UniqueConstraint('checker_id', 'house_id', name='checker_house_unique')
+
+
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+# Request routing:
+
+
+@app.route('/api/houses')
+def index():
+    return jsonify(House.query.all())
+
+
+@app.route('/api/houses/<int:id>/like', methods=['POST'])
+def like(id):
+    req = requests.get(
+        "'https://ed-5424825226493952.educative.run'/api/checker")
+    json = req.json()
+
+    try:
+        houseChecker = HouseChecker(checker_id=json['id'], house_id=id)
+        db.session.add(houseChecker)
+        db.session.commit()
+
+    except:
+        abort(400, 'You have liked this house.')
+
+    return jsonify({
+        'message': 'success'
+    })
+
+
+@app.route('/api/houses/<int:id>/check', methods=['POST'])
+def check(id):
+    req = requests.get(
+        "'https://ed-5424825226493952.educative.run'/api/checker")
+    json = req.json()
+
+    try:
+        houseChecker = HouseChecker(checker_id=json['id'], house_id=id)
+        db.session.add(houseChecker)
+        db.session.commit()
+
+    except:
+        abort(400, 'You have checked this house.')
+
+    return jsonify({
+        'message': 'success'
+    })
 
 
 if __name__ == '__main__':
