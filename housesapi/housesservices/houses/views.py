1fa4f8854a9e69d9fb18ab13c303374b102ff331
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-10 10:06:48 +0530

    Swamy/09may work (#9)
    * Update core.py
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Update core.py
    
    * Changes
    
    * Update views.py

--- housesapi/housesservices/houses/views.py
+++ housesapi/housesservices/houses/views.py
@@ -2,12 +2,20 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.http import JsonResponse
 
 from .models import House
 from .models import Checker
 from .producer import publish
 from .serializers import HouseSerializer
 import random
+
+
+# http://localhost:8000/api/
+def home(request):
+    return JsonResponse({
+        'message': 'Welcome to the API endpoint!'
+    })
 
 
 # Creating the House view:
