9a3dc286e89f20087481d99a1a82041fb923a63a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-14 14:54:12 +0530

    Changes

--- housesweb/src/interfaces/Nav.tsx
+++ housesweb/src/interfaces/Nav.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <nav className='navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow'>
+        <nav className='navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-1 shadow'>
             <a className='navbar-brand col-md-3 col-lg-2 mr-0 px-3' href='#'>
-                Educative INC
+                Houses App
             </a>
             <button
                 className='navbar-toggler position-absolute d-md-none collapsed'
@@ -18,7 +18,7 @@ const Nav = () => {
                 <span className='navbar-toggler-icon'></span>
             </button>
             <input
-                className='form-control form-control-dark w-100'
+                className='form-control form-control-light w-100'
                 type='text'
                 placeholder='Search'
                 aria-label='Search'
