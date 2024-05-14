2af215ea18832fd06a01763f09874f6c66bd3ac9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-14 12:08:47 +0530

    Swamy/13may work (#13)
    * Changes
    
    * Initial Verison
    
    * Changes
    
    * Update package.json
    
    * Changes

--- housesweb/src/App.tsx
+++ housesweb/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './interfaces/Nav';
+import Menu from './interfaces/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Nav />
+
+        <div className='container-fluid'>
+          <div className='row'>
+            <Menu />
+
+            {/* <main role='main' className='col-md-9 ml-sm-auto col-lg-10 px-md-4'>
+              {props.children}
+            </main> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
