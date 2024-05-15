c29ae0f9ceaae1899aa3f32c638dc365b5f4b921
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-15 16:47:23 +0530

    Swamy/14may work (#14)
    * Changes
    
    * Update Nav.tsx
    
    * Update Nav.tsx
    
    * Update Menu.tsx

--- housesweb/src/interfaces/Menu.tsx
+++ housesweb/src/interfaces/Menu.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Menu() {
     return (
-        <nav id='sidebarMenu' className='col-md-3 col-lg-2 d-md-block bg-light sidebar collapse'>
+        <nav id='sidebarMenu' className='col-md-3 col-lg-2 d-md-block bg-secondary sidebar collapse'>
             <div className='sidebar-sticky pt-3'>
                 <ul className='nav flex-column'>
                     <li className='nav-item'>
-                        <a className='nav-link active' href='#'>
+                        <a className='nav-link active text-white' href='/'>
                             Houses
                         </a>
                     </li>
