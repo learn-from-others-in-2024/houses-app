fe42e2a1fb59046fec282b890a7acf749d0e881f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-15 23:17:10 +0530

    Update App.tsx

--- housesweb/src/App.tsx
+++ housesweb/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Houses from './config/Houses';
+import Core from './core/Core';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/' Component={Core} />
           <Route path='/config/houses' Component={Houses} />
         </Routes>
       </BrowserRouter>
