b641d1918ab1a3a26aea2a11530879b59fc6e79e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-16 12:30:34 +0530

    Changes

--- housesweb/src/interfaces/Nav.tsx
+++ housesweb/src/interfaces/Nav.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <nav className='navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-1 shadow'>
-            <a className='navbar-brand col-md-3 col-lg-2 mr-0 px-3' href='/'>
-                Houses App
+        <nav className='navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow'>
+            <a className='navbar-brand col-md-3 col-lg-2 mr-0 px-3' href='#'>
+                Houses
             </a>
             <button
                 className='navbar-toggler position-absolute d-md-none collapsed'
@@ -18,14 +18,14 @@ const Nav = () => {
                 <span className='navbar-toggler-icon'></span>
             </button>
             <input
-                className='form-control form-control-light w-100 rounded'
+                className='form-control form-control-dark w-100 rounded'
                 type='text'
                 placeholder='Search'
                 aria-label='Search'
             />
             <ul className='navbar-nav px-3'>
                 <li className='nav-item text-nowrap'>
-                    <a className='nav-link text-white' href='/'>
+                    <a className='nav-link' href='#'>
                         Sign out
                     </a>
                 </li>
