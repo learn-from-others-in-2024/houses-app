b7f7e4a5e5e3083a33eb885245889903f4d39145
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-18 15:04:13 +0530

    Initial Version

---
+++ housesweb/src/config/HousesUpdate.tsx
@@ -0,0 +1,96 @@
+import React, {
+    PropsWithRef,
+    SyntheticEvent,
+    useEffect,
+    useState,
+} from 'react';
+import Wrapper from './Wrapper';
+import { useNavigate } from 'react-router-dom';
+import { House } from '../interfaces/house';
+
+const HousesUpdate = (props: PropsWithRef<any>) => {
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('');
+    const [description, setDescription] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (redirect) {
+            navigate('/config/houses');
+        }
+    }, [redirect, navigate]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(
+                `http://localhost:8000/api/houses/${props.match.params.id}`
+            );
+
+            const house: House = await response.json();
+
+            setName(house.name);
+            setImage(house.image);
+            setDescription(house.description);
+        })();
+    }, [props.match.params.id]);
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        await fetch(`http://localhost:8000/api/houses/${props.match.params.id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name,
+                image,
+                description,
+            }),
+        });
+
+        setRedirect(true);
+    };
+
+    return (
+        <Wrapper>
+            <form onSubmit={submit}>
+                <div className='form-group'>
+                    <label>Name</label>
+                    <input
+                        placeholder='Name'
+                        type='text'
+                        className='form-control'
+                        name='name'
+                        defaultValue={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label>Image</label>
+                    <input
+                        placeholder='Name'
+                        type='text'
+                        className='form-control'
+                        name='image'
+                        defaultValue={image}
+                        onChange={(e) => setImage(e.target.value)}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label>Description</label>
+                    <input
+                        placeholder='Description'
+                        type='text'
+                        className='form-control'
+                        name='description'
+                        defaultValue={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </div>
+                <button className='btn btn-outline-secondary'>Update</button>
+            </form>
+        </Wrapper>
+    );
+};
+
+export default HousesUpdate;
