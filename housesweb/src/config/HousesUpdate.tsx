b15d40df872ae6ed364854da6987d6e3a2f014cb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-18 15:17:04 +0530

    Update HousesUpdate.tsx

--- housesweb/src/config/HousesUpdate.tsx
+++ housesweb/src/config/HousesUpdate.tsx
@@ -1,14 +1,14 @@
 import React, {
-    PropsWithRef,
     SyntheticEvent,
     useEffect,
     useState,
 } from 'react';
 import Wrapper from './Wrapper';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { House } from '../interfaces/house';
 
-const HousesUpdate = (props: PropsWithRef<any>) => {
+const HousesUpdate = () => {
+    const { id } = useParams<{ id: string }>();
     const [name, setName] = useState('');
     const [image, setImage] = useState('');
     const [description, setDescription] = useState('');
@@ -22,9 +22,10 @@ const HousesUpdate = (props: PropsWithRef<any>) => {
     }, [redirect, navigate]);
 
     useEffect(() => {
+        console.log(id); // Check if the id is correctly retrieved
         (async () => {
             const response = await fetch(
-                `http://localhost:8000/api/houses/${props.match.params.id}`
+                `http://localhost:8000/api/houses/${id}`
             );
 
             const house: House = await response.json();
@@ -33,12 +34,12 @@ const HousesUpdate = (props: PropsWithRef<any>) => {
             setImage(house.image);
             setDescription(house.description);
         })();
-    }, [props.match.params.id]);
+    }, [id]);
 
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
 
-        await fetch(`http://localhost:8000/api/houses/${props.match.params.id}`, {
+        await fetch(`http://localhost:8000/api/houses/${id}`, {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -51,44 +52,33 @@ const HousesUpdate = (props: PropsWithRef<any>) => {
         setRedirect(true);
     };
 
+    const handleCancel = () => {
+        navigate('/config/houses');
+    };
+
     return (
         <Wrapper>
-            <form onSubmit={submit}>
-                <div className='form-group'>
-                    <label>Name</label>
-                    <input
-                        placeholder='Name'
-                        type='text'
-                        className='form-control'
-                        name='name'
-                        defaultValue={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-                <div className='form-group'>
-                    <label>Image</label>
-                    <input
-                        placeholder='Name'
-                        type='text'
-                        className='form-control'
-                        name='image'
-                        defaultValue={image}
-                        onChange={(e) => setImage(e.target.value)}
-                    />
-                </div>
-                <div className='form-group'>
-                    <label>Description</label>
-                    <input
-                        placeholder='Description'
-                        type='text'
-                        className='form-control'
-                        name='description'
-                        defaultValue={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                </div>
-                <button className='btn btn-outline-secondary'>Update</button>
-            </form>
+            <div className='mt-4 rounded shadow p-4'>
+                <h1>Edit House</h1>
+                <form onSubmit={submit}>
+                    <div className='form-group'>
+                        <label>Name</label>
+                        <input placeholder='Name' type='text' className='form-control' name='name' value={name} onChange={(e) => setName(e.target.value)} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Image</label>
+                        <input placeholder='Name' type='text' className='form-control' name='image' value={image} onChange={(e) => setImage(e.target.value)} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Description</label>
+                        <input placeholder='Description' type='text' className='form-control' name='description' value={description} onChange={(e) => setDescription(e.target.value)} />
+                    </div>
+                    <div className='mt-3'>
+                        <button className='btn btn-outline-warning me-2'>Update</button>
+                        <button type='button' className='btn btn-outline-secondary' onClick={handleCancel}>Cancel</button>
+                    </div>
+                </form>
+            </div>
         </Wrapper>
     );
 };
