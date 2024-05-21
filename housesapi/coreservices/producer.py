6a559bf8b3e5fc6fd242d88778c86753e828a103
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-21 17:57:05 +0530

    Swamy/21may work (#18)
    * Changes
    
    * Changes
    
    * Update consumer.py
    
    * Update core.py
    
    * Changes

--- housesapi/coreservices/producer.py
+++ housesapi/coreservices/producer.py
@@ -9,14 +9,26 @@ load_dotenv()
 # Input your rabbitmq url
 params = pika.URLParameters(os.getenv("RABBIT_AMQP_URL"))
 
-# Create a connection
-connection = pika.BlockingConnection(params)
-
-channel = connection.channel()
-
 
 # Create a publisher
 def publish(method, body):
+    print('Publishing to -> Config Service')
+
+    # Create a connection
+    connection = pika.BlockingConnection(params)
+
+    # Create a channel
+    channel = connection.channel()
+
+    # Set properties
     properties = pika.BasicProperties(method)
+    print('Config Service -> publish() :: Properties: ',
+          properties, ' Body: ', body)
+
+    # Publish message
     channel.basic_publish(exchange='', routing_key='config',
                           body=json.dumps(body), properties=properties)
+
+    # Close the channel and connection
+    channel.close()
+    connection.close()
